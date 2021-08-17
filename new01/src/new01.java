13d6849cb0a01fce58318a6dfa64fcbd5e001972
CA2528357431 <2528357431@QQ.com>
2021-08-17 20:56:29 +0800

    first commit

---
+++ new01/src/new01.java
@@ -0,0 +1,18 @@
+import java.sql.Time;
+
+public class new01 {
+    public static void main(String[] args){
+
+        long time = System.currentTimeMillis();
+        System.out.println(time);
+        // 时间戳
+
+        System.exit(0);
+        // 结束
+        // 参数是结束状态码
+
+        System.out.println(time);
+
+
+    }
+}
